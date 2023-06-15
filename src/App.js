6e0bc9892a9dc6f9c9ba1e7623f9fa71c7b2b527
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-06-15 14:50:48 -0300

    Em desenvolvimento
    Cronômetro criado para aprendizado de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      numero: 0
+    }
+  }
+
+  render(){
+    return(
+      <div className='container'>
+        <img src={require('./assets/cronometro.png')} className='img'/> {/*Inserindo a imagem de fundo do cronômetro */}
+        <a className='timer'>{this.state.numero.toFixed(1)}</a>{/*Com  o uso da javascript inserimos a quantidade de casas decimais que desejamos, neste caso apenas uma */}
+
+        {/*Inserindo os botões */}
+        <div className='areaBtn'>
+          <a className='botao'>VAI</a>
+          <a className='botao'>LIMPAR</a>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
