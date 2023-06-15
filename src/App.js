0987cac96448f90e25de9bd5e5bbacb5d4bf7179
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-06-15 15:58:15 -0300

    Finalizado
    Projeto cronômetro finalizado

--- src/App.js
+++ src/App.js
@@ -6,7 +6,46 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      numero: 0
+      numero: 0,
+      botao: 'VAI'
+    }
+
+    this.timer = null;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  vai(){
+
+    let state = this.state
+
+    if(this.timer !== null){
+      clearInterval(this.timer)
+      this.timer = null
+      state.botao = 'VAI'
+      
+    }else{
+       
+      this.timer = setInterval(()=>{
+      let state = this.state
+      state.numero += 0.1
+      this.setState(state)
+
+    },100)
+      state.botao = 'PAUSAR'
+    }
+    this.setState(state)
+  }
+
+  limpar(){
+    if(this.timer !== null){
+      clearInterval(this.timer)
+      this.timer = null
+    }else{
+      let state = this.state
+      this.state.numero = 0
+      state.botao = 'VAI'
+      this.setState(state)
     }
   }
 
@@ -18,8 +57,8 @@ class App extends Component{
 
         {/*Inserindo os botões */}
         <div className='areaBtn'>
-          <a className='botao'>VAI</a>
-          <a className='botao'>LIMPAR</a>
+          <a className='botao' onClick={this.vai}>{this.state.botao}</a>
+          <a className='botao' onClick={this.limpar}>LIMPAR</a>
         </div>
       </div>
     )
